9b7d8d18275e193dc587b3c9eb21a5466c09a0c9
Eiji Kumakawa <morinokumakamesan@gmail.com>
2019-10-13 18:23:54 +0900

    スクリーン用ページの追加

---
+++ pages/screen.vue
@@ -0,0 +1,95 @@
+<template>
+  <section class="section">
+    
+    <div v-if="showModal_re">
+			正解は{{ question.answer }}
+    </div>
+
+    <div id="wrapper" class="container">
+      <b-loading :is-full-page="false" :active.sync="isLoading" :can-cancel="false"></b-loading>
+      <p>{{ question.num }} {{ question.content }}</p>
+    </div>
+    <div v-if="top">
+      <p>クイズ大会だよ〜</p>
+      <p>みんなが入るまで待っててね</p>
+    </div>
+    <div v-else>
+      <button class="button is-info" @click="answer('1')">1</button>
+      <button class="button is-info" @click="answer('2')">2</button>
+      <button class="button is-info" @click="answer('3')">3</button>
+      <button class="button is-info" @click="answer('4')">4</button>
+    </div>
+  </section>
+</template>
+
+<script>
+import io from 'socket.io-client'
+import questions from '../assets/api/question.json'
+import loading from '~/components/Loading.vue'
+import eachResult from '~/components/eachResult.vue'
+
+export default {
+  components: {
+    loading,
+    eachResult
+  },
+  data() {
+    return {
+      socket: '',
+      isLoading: true,
+      questions: questions,
+      question: '',
+      loading: false,
+      showModal: false,
+      showModal_re: false,
+      top: true,
+      name: '',
+      corNum: 0,
+      corNum_before: 0,
+      ans: {
+        id: '',
+        ans: '',
+        correct: false,
+      }
+    }
+  },
+  mounted() {
+    // VueインスタンスがDOMにマウントされたらSocketインスタンスを生成する
+    this.socket = io()
+
+    // 問題の受け取り
+    this.socket.on('Question', question => {
+      this.question = questions[question.id]
+      console.log('ボタンおされたよ2')
+    })
+
+    // 回答トリガの受け取り
+    this.socket.on('eachResult', result => {
+      this.showModal_re = result
+      console.log('ボタンおされたよ')
+    })
+
+    // コンポーネントがマウントされてから1秒間はローディングする
+    setTimeout(() => {
+      this.isLoading = false
+    }, 1000)
+  },
+  methods: {
+  },
+  watch: {
+    question: function(){
+      this.showModal_re = false
+      this.top = (this.question.id == 0) ? true : false
+      this.corNum_before = this.corNum
+      console.log(this.top)
+    }
+  }
+}
+</script>
+
+<style scoped>
+#wrapper
+{
+  max-width: 600px;
+}
+</style>
