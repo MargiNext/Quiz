6e00400d70359e7dd418e567f70d4d3c06295c34
Eiji Kumakawa <morinokumakamesan@gmail.com>
2019-11-02 10:34:19 +0900

    一旦

--- pages/screen.vue
+++ pages/screen.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="section">
     
-    <!-- <div v-if="show">
+    <div v-if="show">
 			正解は{{ question.answer }}
-    </div> -->
+    </div>
 
     <div v-if="top">
       <p>クイズ大会だよ〜</p>
@@ -54,9 +54,6 @@ export default {
   },
   data() {
     return {
-			styleObject: {
-				fontSize: '13px'
-			},
       color_1: 'background-color: transparent; border-color: #209cee; color: #209cee;',
       color_2: 'background-color: transparent; border-color: #3273dc; color: #3273dc;',
       color_3: 'background-color: transparent; border-color: #00d1b2; color: #00d1b2;',
@@ -106,7 +103,7 @@ export default {
 
     // 回答トリガの受け取り
     this.socket.on('eachResult', result => {
-      this.show = result
+			this.show = result
 		})
 
     // 最終結果発表トリガの受け取り
@@ -127,7 +124,6 @@ export default {
 			this.rateShow = false
       this.top = (this.question.id == 0) ? true : false
       this.corNum_before = this.corNum
-      console.log(this.top)
     }
   }
 }
