9016a621a63d24991bc9ce5734fb5d1b9a035675
Eiji Kumakawa <morinokumakamesan@gmail.com>
2019-11-18 21:25:40 +0900

    attachment designs 1

--- pages/login.vue
+++ pages/login.vue
@@ -1,17 +1,23 @@
 <template>
-  <section class="section">
-      <p>ろぐいん</p>
+  <!-- <section class="section"> -->
       <div class="field">
-        <label class="label">Team Name</label>
-        <div class="control">
-            <input class="input" type="text" placeholder="Text input" v-model="name">
+        <div class="is-mobile" id="padding_u_100">
+          <p style="text-align: center; font-size: 30px;">SI部</p>
+          <p style="text-align: center; font-size: 40px;">大忘年会</p>
         </div>
-        <p v-if='isLogin' style="color: red;">※このグループ名はすでに使用済みです</p>
-        <div style="padding: 20px 0 0;">
-          <a class="button is-primary" @click="login()">ユーザを作成しログイン</a>
+        <div class="columns is-mobile" id="padding_u_100">
+          <div :class="box">
+            <input class="input" type="text" placeholder="Team Name" v-model="name">
+            <p v-if='isLogin' style="color: red;">※このグループ名はすでに使用済みです</p>
+          </div>
         </div>
-			</div>
-  </section>
+        <div class="columns is-mobile">
+          <div :class="box">
+            <a class="button is-primary" style="width: 100%;" @click="login()">ユーザを作成しログイン</a>
+          </div>
+        </div>
+      </div>
+  <!-- </section> -->
 </template>
 
 <script>
@@ -25,6 +31,7 @@ export default {
       name: '',
       isLogin: true,
       Login: [false],
+      box: 'column is-10 is-offset-1'
     }
   },
   mounted() {
@@ -63,4 +70,7 @@ export default {
 </script>
 
 <style scoped>
+#padding_u_100 {
+  padding: 100px 0px 0px;
+}
 </style>
